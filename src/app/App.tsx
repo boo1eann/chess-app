725bfea2367d482c67d5b5b00363e0e2402a5dbd
Damir <damircodenn@gmail.com>
2026-05-26 01:26:54 +0500

    feat: add query provider (react-query)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,18 @@ import { useBootstrapAuth } from "@/features/auth/hooks/useBootstrapAuth";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { RootNavigator } from "./navigation/RootNavigator";
+import { QueryProvider } from "./providers/QueryProvider";
 
 export default function App() {
   useBootstrapAuth();
 
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <QueryProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </QueryProvider>
     </SafeAreaProvider>
   );
 }
